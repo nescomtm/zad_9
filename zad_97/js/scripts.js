ea0457cbf0b9dc93edc27197999e279d392ca8d6
nescomtm <tomczyk@nescom.co>
2017-05-04 09:27:52 +0200

    zad_97

---
+++ zad_97/js/scripts.js
@@ -0,0 +1,152 @@
+var newGameBtn = document.getElementById('js-newGameButton');
+
+newGameBtn.addEventListener('click', newGame);
+
+var pickRock = document.getElementById('js-playerPick_rock'),
+     pickPaper = document.getElementById('js-playerPick_paper'),
+     pickScissors = document.getElementById('js-playerPick_scissors');
+
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
+
+var gameState = 'notStarted',  //started // ended
+    player = {
+        name: '',
+        score: 0
+    },
+    computer = {
+        score: 0
+    };
+
+ var newGameElem = document.getElementById('js-newGameElement'),
+    pickElem = document.getElementById('js-playerPickElement'),
+    resultsElem = document.getElementById('js-resultsTableElement');
+
+function setGameElements() {
+    switch(gameState) {
+        case 'started':
+            newGameElem.style.display = 'none';
+            pickElem.style.display = 'block';
+            resultsElem.style.display = 'block';
+            break;
+        case 'ended':
+            newGameBtn.innerText = 'Jeszcze raz';
+        case 'notStarted':
+        default:
+            newGameElem.style.display = 'block';
+            pickElem.style.display = 'none';
+            resultsElem.style.display = 'none';
+  }
+}
+
+setGameElements();
+
+var playerPointsElem = document.getElementById('js-playerPoints'),
+    playerNameElem = document.getElementById('js-playerName'),
+    computerPointsElem = document.getElementById('js-computerPoints');
+
+function newGame() {
+  player.name = prompt('Graczu, wpisz swoje imię', 'imię gracza');
+  if (player.name) {
+      player.score = computer.score = 0;
+      gameState = 'started';
+      setGameElements();
+
+     // playerPointsElem.innerHTML = player.score;  //moje
+      
+      playerNameElem.innerHTML = player.name;
+      setGamePoints(); 
+  }
+}
+
+function playerPick(playerPick) {
+    console.log(playerPick);
+}
+
+//var x = Math.random();
+
+
+function getComputerPick() {
+    var possiblePicks = ['rock', 'paper', 'scissors'];
+    return possiblePicks[Math.floor(Math.random()*3)];
+}
+
+var playerPickElem = document.getElementById('js-playerPick'),
+    computerPickElem = document.getElementById('js-computerPick'),
+    playerResultElem = document.getElementById('js-playerResult'),
+    computerResultElem = document.getElementById('js-computerResult');
+
+function playerPick(playerPick) {
+    var computerPick = getComputerPick();
+    
+    playerPickElem.innerHTML = playerPick;
+    computerPickElem.innerHTML = computerPick;
+}
+
+function checkRoundWinner(playerPick, computerPick) {
+  playerResultElem.innerHTML = computerResultElem.innerHTML = '';
+
+  var winnerIs = 'player';
+
+    if (playerPick == computerPick) {
+        winnerIs = 'noone'; // remis
+    } else if (
+        (computerPick == 'rock' &&  playerPick == 'scissors') ||
+        (computerPick == 'scissors' &&  playerPick == 'paper') ||
+        (computerPick == 'paper' &&  playerPick == 'rock')) {
+        
+        winnerIs = 'computer';
+    }
+
+    if (winnerIs == 'player') {
+        playerResultElem.innerHTML = "Wygrana!";
+        player.score++;
+        
+        //console.log(player.score);
+        setGamePoints();
+
+    } else if (winnerIs == 'computer') {
+        computerResultElem.innerHTML = "Wygrana!";
+        computer.score++;
+
+        //console.log(computer.score);
+        setGamePoints();
+    }
+
+}
+
+function playerPick(playerPick) {
+    var computerPick = getComputerPick();
+    
+    playerPickElem.innerHTML = playerPick;
+    computerPickElem.innerHTML = computerPick;
+    
+    checkRoundWinner(playerPick, computerPick);
+}
+
+function setGamePoints() {
+    playerPointsElem.innerHTML = player.score;
+    computerPointsElem.innerHTML = computer.score;
+    //console.log(player.score);
+    //console.log(computer.score);
+    if (player.score == 10) {
+        playerResultElem.innerHTML = "Wygrana! Pana "+player.name;
+        alert("Wygrana! Pana "+player.name);
+        alert("Nacisnij OK aby rozpocząć gre od nowa");
+        newGame();
+        setGameElements();
+        
+    } else if (computer.score == 10) {
+        computerResultElem.innerHTML = "Wygrana! Pana Computer - koniec gry";
+        alert("Wygrana! Pana Computer - koniec gry");
+        alert("Nacisnij OK aby rozpocząć gre od nowa");
+        newGame();
+        setGameElements();
+        
+    }
+}
+
+// z tych dwoch zmiennych nalezy wyprowadzic wyniki do przegladarki zrobione
+
+//runda ma trwac do 10 zwycienst
