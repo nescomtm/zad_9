4a6a6272b5402eeb370b8e60b5b027c613ea3861
nescomtm <tomczyk@nescom.co>
2017-05-04 09:26:39 +0200

    zad_96

---
+++ zad_96/js/scripts.js
@@ -0,0 +1,17 @@
+//var przyciskNaStronie.addEventListener(click,handler);
+var list = document.getElementById('list');
+var add = document.getElementsByTagName("button");
+button.addEventListener('click',myFunctionAdd(){});
+
+console.log(list);
+console.log(add);
+
+
+function myFunctionAdd () {
+    numer = (getElementsByTagName);
+    list.innerHTML += '<li>item '+numer+'</li>';
+}
+
+
+
+myFunctionAdd();
