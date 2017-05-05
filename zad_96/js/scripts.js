a2f391d13f4002ce0e1f7b43652619a2a5e7da90
nescomtm <tomczyk@nescom.co>
2017-05-05 23:00:20 +0200

    zad 96 pop3

--- zad_96/js/scripts.js
+++ zad_96/js/scripts.js
@@ -1,20 +1,7 @@
-//var przyciskNaStronie.addEventListener(click,handler);
 var list = document.getElementById('list');
-
 var add = document.getElementById('addElem');
-
-//var liczba = 0;
 
 add.addEventListener('click', function() {
     var liczba = document.getElementsByTagName('li').length;
-    
-    list.innerHTML += '<li>item '+liczba+'</li>';
+    list.innerHTML += '<li>item ' + liczba + '</li>';
 });
-
-//console.log(liczba);
-console.log(list);
-console.log(add);
-
-
-
-
