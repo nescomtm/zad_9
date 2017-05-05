b3ff23266f9fca2b9de9819f91a916831112cf54
nescomtm <tomczyk@nescom.co>
2017-05-05 22:55:46 +0200

    zad 95 pop3

--- zad_95/js/scripts.js
+++ zad_95/js/scripts.js
@@ -6,6 +6,6 @@ for (var i = 0; i < dlugosc; i++) {
     console.log(itemByClassName[i]);
     console.log(itemByClassName[i].innerText); // podejrzałem na necie jak to uzyć :)
 
-    alert(itemByClassName[i]);
+    //alert(itemByClassName[i]);
     alert(itemByClassName[i].innerText);
 }
