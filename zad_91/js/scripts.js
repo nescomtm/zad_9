82d462eaa05acfafa06e9a31c2ac6a0af49bb349
Michał Dolny <michal.dolny@gmail.com>
2017-05-05 17:56:51 +0200

    ort

--- zad_91/js/scripts.js
+++ zad_91/js/scripts.js
@@ -1,8 +1,8 @@
 function getTriangleArea(a,h) {
     if (a <= 0 || h <= 0) {
-        console.log('nie prawidłowe dane - zmienne sa mniejsze od zera');
+        console.log('nieprawidłowe dane - zmienne sa mniejsze od zera');
     } else {
-        return a*h/2;        
+        return a*h/2;
     }
 }
 
@@ -12,7 +12,7 @@ console.log(getTriangleArea(5,8));   //wynik ma byc 20
 
 console.log(getTriangleArea(20,23));   //wynik ma byc 230
 
-console.log(getTriangleArea(0, 6)); 
+console.log(getTriangleArea(0, 6));
 
 console.log(getTriangleArea(10, 0));
 
